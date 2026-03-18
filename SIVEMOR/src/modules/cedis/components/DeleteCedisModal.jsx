c1925f3c997b8bd45a45e60b178d368c0f33b412
Oscar-F-M <20243ds081@utez.edu.mx>
2026-03-18 01:32:25 -0600

    Creacion de Cedis, con los modals necesarios, crear cedis, eliminar y guardar

---
+++ SIVEMOR/src/modules/cedis/components/DeleteCedisModal.jsx
@@ -0,0 +1,47 @@
+export default function DeleteCedisModal({
+  selectedCount,
+  totalCount,
+  onDelete,
+}) {
+  return (
+    <div className="modal fade" id="deleteCedisModal" tabIndex="-1" aria-hidden="true">
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Confirmar Eliminación</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+          </div>
+
+          <div className="modal-body">
+            {selectedCount === totalCount ? (
+              <>
+                ¿Estás seguro de que deseas eliminar los <strong>CEDIS</strong> seleccionados?
+                Esta acción no se puede deshacer.
+              </>
+            ) : (
+              <>
+                ¿Estás seguro de que deseas eliminar <strong>{selectedCount}</strong> CEDIS
+                seleccionado{selectedCount > 1 ? "s" : ""}? Esta acción no se puede deshacer.
+              </>
+            )}
+          </div>
+
+          <div className="modal-footer">
+            <button type="button" className="btn btn-light" data-bs-dismiss="modal">
+              Cancelar
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-danger"
+              data-bs-dismiss="modal"
+              onClick={onDelete}
+            >
+              Eliminar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
