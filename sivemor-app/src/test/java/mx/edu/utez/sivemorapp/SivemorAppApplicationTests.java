90ed1d930769aa84cf10d2630ce763f258850549
lydvnieel <20243ds183@utez.edu.mx>
2026-03-28 18:25:42 -0600

    Agregue carpetas de las entidades de la base de datos

---
+++ sivemor-app/src/test/java/mx/edu/utez/sivemorapp/SivemorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.sivemorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SivemorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
