37ae752cc84c351e3d69c6a09e10271d40fb7215
lydvnieel <20243ds183@utez.edu.mx>
2026-04-02 19:32:36 -0600

    VehiculoController

--- sivemor-app/src/main/java/mx/edu/utez/sivemorapp/modules/vehiculos/VehiculoController.java
+++ sivemor-app/src/main/java/mx/edu/utez/sivemorapp/modules/vehiculos/VehiculoController.java
@@ -1,4 +1,48 @@
 package mx.edu.utez.sivemorapp.modules.vehiculos;
 
+import lombok.RequiredArgsConstructor;
+import mx.edu.utez.sivemorapp.kernel.ApiResponse;
+import mx.edu.utez.sivemorapp.modules.vehiculos.dtos.VehiculoRequestDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sivemor/api/vehiculos")
+@RequiredArgsConstructor
 public class VehiculoController {
+
+    private final VehiculoService vehiculoService;
+
+    @GetMapping("")
+    public ResponseEntity<ApiResponse> findAllVehiculos(
+            @RequestParam(value = "idCliente", required = false) Long idCliente,
+            @RequestParam(value = "idCedis", required = false) Long idCedis,
+            @RequestParam(value = "placa", required = false) String placa
+    ) {
+        return vehiculoService.filterVehiculos(idCliente, idCedis, placa);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse> findVehiculoById(@PathVariable("id") Long id) {
+        return vehiculoService.findVehiculoById(id);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ApiResponse> saveVehiculo(@RequestBody VehiculoRequestDTO dto) {
+        return vehiculoService.saveVehiculo(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> updateVehiculo(
+            @PathVariable("id") Long id,
+            @RequestBody VehiculoRequestDTO dto
+    ) {
+        dto.setId(id);
+        return vehiculoService.updateVehiculo(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteVehiculo(@PathVariable("id") Long id) {
+        return vehiculoService.deleteVehiculo(id);
+    }
 }
