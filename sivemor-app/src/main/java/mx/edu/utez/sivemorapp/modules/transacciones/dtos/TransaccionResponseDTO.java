a5588e9a9e7cf426e46b3223f770fb65b68fb705
lydvnieel <20243ds183@utez.edu.mx>
2026-04-03 18:55:17 -0600

    agregue cambios para mostrar nombre de empleado que atendio

--- sivemor-app/src/main/java/mx/edu/utez/sivemorapp/modules/transacciones/dtos/TransaccionResponseDTO.java
+++ sivemor-app/src/main/java/mx/edu/utez/sivemorapp/modules/transacciones/dtos/TransaccionResponseDTO.java
@@ -14,10 +14,28 @@ public class TransaccionResponseDTO {
 
     private Long id;
     private Long idNota;
+    private String folioNota;
+
     private TipoPago tipoPago;
     private BigDecimal monto;
+    private String cuentaDeposito;
     private String numeroFactura;
+
     private Boolean pagado;
     private Boolean pendiente;
+
     private LocalDateTime fechaPedido;
+    private String cotizacion;
+
+    private Long reviso;
+    private String nombreReviso;
+
+    private Long atendio;
+    private String nombreAtendio;
+
+    private String comentario;
+
+    private Boolean activo;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- sivemor-app/src/main/java/mx/edu/utez/sivemorapp/modules/transacciones/dtos/utils/TransaccionMapper.java
+++ sivemor-app/src/main/java/mx/edu/utez/sivemorapp/modules/transacciones/dtos/utils/TransaccionMapper.java
@@ -11,17 +11,28 @@ public class TransaccionMapper {
     public static TransaccionResponseDTO toDto(Transaccion t) {
         return TransaccionResponseDTO.builder()
                 .id(t.getId())
-                .idNota(t.getNota().getId())
+                .idNota(t.getNota() != null ? t.getNota().getId() : null)
+                .folioNota(t.getNota() != null ? t.getNota().getFolioNota() : null)
                 .tipoPago(t.getTipoPago())
                 .monto(t.getMonto())
+                .cuentaDeposito(t.getCuentaDeposito())
                 .numeroFactura(t.getNumeroFactura())
                 .pagado(t.getPagado())
                 .pendiente(t.getPendiente())
                 .fechaPedido(t.getFechaPedido())
+                .cotizacion(t.getCotizacion())
+                .reviso(t.getReviso() != null ? t.getReviso().getId() : null)
+                .nombreReviso(t.getReviso() != null ? t.getReviso().getNombreUsuario() : null)
+                .atendio(t.getAtendio() != null ? t.getAtendio().getId() : null)
+                .nombreAtendio(t.getAtendio() != null ? t.getAtendio().getNombreUsuario() : null)
+                .comentario(t.getComentario())
+                .activo(t.getActivo())
+                .createdAt(t.getCreatedAt())
+                .updatedAt(t.getUpdatedAt())
                 .build();
     }
 
-    public static List<TransaccionResponseDTO> toList(List<Transaccion> list) {
+    public static List<TransaccionResponseDTO> toDtoList(List<Transaccion> list) {
         return list.stream().map(TransaccionMapper::toDto).collect(Collectors.toList());
     }
 }
